6335ecaa046ba2d90f161fdf4b808a2dae72c080
yukehi <63537531+yukehi@users.noreply.github.com>
2020-10-08 20:07:32 +0300

    blank stage

---
+++ Reset/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reset
+//
+//  Created by Aleksey zgeria on 08/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
